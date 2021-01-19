be05344e26a10dd37a0d77599c57eacb60f5cf5e
ROSHAN PATEL <roshanpatel@ROSHANs-MacBook-Pro.local>
2021-01-19 15:45:18 -0500

    Homework for January 15, 2021

---
+++ kavish/Homework_Jan 16 2021.py
@@ -0,0 +1,84 @@
+#1. Print your name in the python interpreter window.
+print("#1. Print your name in the python interpreter window.")
+print("Kavish Patel")
+
+#2. Print your favourite movie name.
+print("#2. Print your favourite movie name.")
+print("3 Idiots")
+
+
+print("#3. Print the name of your school.")
+print("The Lindeneau Elementary School")
+
+#4. Create appropriate variables for above 1,2 and 3 questions
+name = "Kavish Patel"
+movie = "3 Idiots"
+school = "The Lindeneau Elementary School"
+
+#5 Print the varaiables created in question 4
+print("#5 Print the varaiables created in question 4")
+print("Name = " + name)
+print("Movie = "+ movie)
+print("School = " + school)
+
+#6 Assign / store variables created in question 4 in a new variable. print the variable e.g. print(variable1+variable2+variable3)
+n = name;
+m = movie;
+s = school;
+
+print("#6 Assign / store variables created in question 4 in a new variable. print the variable e.g. print(variable1+variable2+variable3)")
+print("Name = " + n)
+print("Movie = "+ m)
+print("School = " + s)
+
+#7 There are 20 coins that you found when diving deep into an ocean. You found another 10 coins that were magical 10. When you came out of ocean, it seems that 3 coins were lost. Print how many total coins you have left.
+coins = 20
+magicalcoins = 10
+lostcoins = 3
+totalcoins = 0
+
+totalcoins = coins + magicalcoins - lostcoins;
+print("#7 There are 20 coins that you found when diving deep into an ocean. You found another 10 coins that were magical 10. When you came out of ocean, it seems that 3 coins were lost. Print how many total coins you have left.")
+print(totalcoins)
+
+#8 Create 2 variables and assign any number to each of them
+number1 = 100;
+number2 = 20;
+
+#a. Add 2 variables
+print("#a. Add 2 variables")
+print(number1 + number2)
+
+#b. Subtract 2 variables
+print("#b. Subtract 2 variables")
+print(number1 - number2)
+
+#c. Multiply 2 variables
+print("#c. Multiply 2 variables")
+print(number1 * number2)
+
+#d. Divide 2 variables
+print("#d. Divide 2 variables")
+print(number1 / number2)
+
+#e. Optional : if you want to practice more, create 3 variables and repeat a through d.
+a = 100;
+b = 50;
+c = 10;
+
+#a. Add 3 variables
+print("#a. Add 3 variables")
+print(a + b + c)
+
+#b. Subtract 3 variables
+print("#b. Subtract 3 variables")
+print(a - b - c)
+
+#c. Multiply 3 variables
+print("#c. Multiply 3 variables")
+print(a * b * c)
+
+#d. Divide 3 variables
+print("#d. Divide 3 variables")
+print(a / b / c)
+
